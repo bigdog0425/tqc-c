99ad47ac5db7b747149d71611c46f0999034f09e
bigdog0425 <39252443+bigdog0425@users.noreply.github.com>
2018-06-07 09:29:48 +0800

    Update 902.c

--- 902.c
+++ 902.c
@@ -6,15 +6,18 @@
 //902 解答參考
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#define MAX 6
-#define RANGE 49
+#include <time.h> //使用time函數
+#define MAX 6 //定義最大隨機數字個數
+#define RANGE 49 //定義最大隨機數字範圍
+
 void main(){
-    int a[MAX],i,j;
-    srand(time(NULL));
+    int a[MAX],i,j; //a[MAX]：存放隨機數的陣列
+    srand(time(NULL)); //隨機產生亂數種子
     for(i=0;i<MAX;i++){
-        a[i]=(rand()%RANGE)+1;
-        for(j=i-1;j>=0;j--)
+        a[i]=(rand()%RANGE)+1; //隨機取出(0~48)+1的數字，既隨機取出1~49數字
+
+        //去除重複數字：
+        for(j=i-1;j>=0;j--) 
             if(a[i]==a[j])
                 i--;
     }
