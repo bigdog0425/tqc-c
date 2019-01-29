2e8700041f7fe9eb504e7ecabae83929db264029
海青大狗哥 <39252443+bigdog0425@users.noreply.github.com>
2019-01-29 09:12:08 +0800

    Update 102-110.c

--- 102-110.c
+++ 102-110.c
@@ -52,19 +52,18 @@ int main()
 //104解答參考
 #include <stdio.h>
 #include <stdlib.h>
- 
-int main()
+
+int main() 
 {
-    int unit;
-    float price=23.34;
-    float total;
-    printf("請問您要買幾瓶蘋果汁? ");
-    scanf("%d", &unit);
-    total = (float)unit*price;
-    printf("我買了%d瓶100%%的蘋果汁\n", unit);
-    printf("花了%f元", total);
-    system("PAUSE");
-    return 0;
+     int unit;
+     double price=23.34; 
+     double total;
+     printf("請問您要買幾瓶蘋果汁? ");
+     scanf("%d", &unit);
+     total = unit*price;
+     printf("我買了%d瓶100%的蘋果汁\n", unit);
+     printf("花了%lf元", total);
+     return 0;
 }
  
 /*-------------------------------------------------------------*/
